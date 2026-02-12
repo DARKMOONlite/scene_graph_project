7d169e4cb8e84a670ecb5ffed2a32cff639abddd
Sebastian <Sebastian.Schroder@sperospace.com>
2026-02-12 13:07:28 +1100

    added functions for modifying with anyBURL rule files

---
+++ rule_mining/datasets/visual_genome/split_scene_graph_file.py
@@ -0,0 +1,9 @@
+import visual_genome.local as vg
+
+
+vg.save_scene_graphs_by_id(data_dir="data/",image_data_dir="data/by-id/")
+
+scene_graphs = vg.get_scene_graphs(start_index=0, end_index=-1, min_rels=1,
+                                data_dir='data/', image_data_dir='data/by-id/')
+print(len(scene_graphs))
+print(scene_graphs[0].objects)
